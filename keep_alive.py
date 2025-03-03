27209f1d42b93534ce725cfd3c301be6cd2c7f4a
JangJinOne <jangjinone@gmail.com>
2025-03-03 09:31:44 +0900

    Remove ping, use instead of uptimeRobot

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from threading import Thread
-import requests
-import time
 
 app = Flask('')
 
@@ -12,17 +10,6 @@ def home():
 def run():
     app.run(host='0.0.0.0', port=8080)
 
-def ping():
-    while True:
-        try:
-            requests.get("http://127.0.0.1:8080/")
-        except requests.exceptions.RequestException:
-            print("Failed to reach the server.")
-        time.sleep(30)  # 30초마다 호출
-
 def keep_alive():
     t = Thread(target=run)
     t.start()
-
-    t2 = Thread(target=ping, daemon=True)
-    t2.start()
